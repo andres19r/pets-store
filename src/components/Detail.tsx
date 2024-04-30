dc1532c86098a481022d26debe319790936ebbd5
Andres Rivero <andres19rivero@gmail.com>
2024-04-30 11:28:12 -0400

    feat(components): implement Pet's Detail

--- src/components/Detail.tsx
+++ src/components/Detail.tsx
@@ -1,5 +1,29 @@
-function Detail() {
-  return <div>Detail</div>;
+import { Card, CardContent, CardMedia, Typography } from "@mui/material";
+
+export interface PetContent {
+  name: string;
+  type: string;
+  sex: string;
+  imgUrl: string;
+}
+
+function Detail({ name, type, sex, imgUrl }: PetContent) {
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia sx={{ height: 140 }} image={imgUrl} title={`name ${imgUrl}`} />
+      <CardContent>
+        <Typography gutterBottom variant="h3" component="div">
+          {name}
+        </Typography>
+        <Typography gutterBottom variant="h4" component="div">
+          {type}
+        </Typography>
+        <Typography gutterBottom variant="h5" component="div">
+          {sex}
+        </Typography>
+      </CardContent>
+    </Card>
+  );
 }
 
 export default Detail;
