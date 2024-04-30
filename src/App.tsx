0155d4fd671ad3c114dd5ee48a7a605a05488416
Andres Rivero <andres19rivero@gmail.com>
2024-04-30 11:27:48 -0400

    feat: register routes & use store in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import List from "./components/List";
 import Create from "./components/Create";
 
 function App() {
-  return <Create />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<List />}></Route>
+        <Route path="/register" element={<Create />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
