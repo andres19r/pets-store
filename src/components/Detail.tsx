e754a299dd81dd671043a9f35c3d810c9cffc660
Andres Rivero <andres19rivero@gmail.com>
2024-04-30 11:54:23 -0400

    feat(Detail): implement modal to check pet's full details

--- src/components/Detail.tsx
+++ src/components/Detail.tsx
@@ -1,4 +1,14 @@
-import { Card, CardContent, CardMedia, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Modal,
+  Typography,
+} from "@mui/material";
+import { useState } from "react";
 
 export interface PetContent {
   name: string;
@@ -7,21 +17,47 @@ export interface PetContent {
   imgUrl: string;
 }
 
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
+
 function Detail({ name, type, sex, imgUrl }: PetContent) {
+  const [open, setOpen] = useState(false);
   return (
     <Card sx={{ maxWidth: 345 }}>
-      <CardMedia sx={{ height: 140 }} image={imgUrl} title={`name ${imgUrl}`} />
       <CardContent>
         <Typography gutterBottom variant="h3" component="div">
           {name}
         </Typography>
-        <Typography gutterBottom variant="h4" component="div">
-          {type}
-        </Typography>
-        <Typography gutterBottom variant="h5" component="div">
-          {sex}
-        </Typography>
       </CardContent>
+      <CardActions>
+        <Button onClick={() => setOpen(true)} variant="contained" size="small">
+          Details
+        </Button>
+
+        <Modal open={open} onClose={() => setOpen(false)}>
+          <Box sx={style}>
+            <img src={imgUrl} alt={name + " image"} />
+            <Typography id="modal-modal-title" variant="h6" component="h2">
+              {name}
+            </Typography>
+            <Typography gutterBottom variant="h4" component="div">
+              {type}
+            </Typography>
+            <Typography gutterBottom variant="h5" component="div">
+              {sex}
+            </Typography>
+          </Box>
+        </Modal>
+      </CardActions>
     </Card>
   );
 }
